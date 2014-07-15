cbe63705e7e9bee3541b21b826261cb16415f610
Robert Quattlebaum <darco@deepdarc.com>
2014-07-14 19:23:23 -0700

    blah

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -480,6 +480,9 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 
 // default settings
 // delta speeds must be the same on xyz
+// Calculated steps-per-unit is:
+// (STEPS_PER_REVOLUTION)/(SPOOL_DIAMETER*M_PI) == (200*16)/(18*M_PI) == 56.588
+// Not sure why I measured 54.47... Which would be the equivalent of a 18.7mm diameter spool... hmm...
 #define DEFAULT_AXIS_STEPS_PER_UNIT   {54.47, 54.47, 54.47, 592}
 #define DEFAULT_MAX_FEEDRATE          {500, 500, 500, 100}    // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {1500,1500,1500,100}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for skeinforge 40+, for older versions raise them a lot.
